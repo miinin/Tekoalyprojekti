daa7a5c713872d26a6241299c66105ea87ffed00
miinin <miika.miinin@edu.lappeenranta.fi>
2026-03-27 14:19:34 +0200

    valkoinen pois

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Roadmap from './pages/Roadmap';
 import Garage from './pages/Garage';
