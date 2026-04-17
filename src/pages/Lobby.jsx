6ea3e1225a3fd7a079e1ba60340bc14bd45eb87d
miinin <miika.miinin@edu.lappeenranta.fi>
2026-04-17 13:19:40 +0300

    lobbyfix

--- src/pages/Lobby.jsx
+++ src/pages/Lobby.jsx
@@ -291,7 +291,7 @@ export default function Lobby() {
                 <form onSubmit={handleRestoreCloudSave} style={{ display: 'flex', gap: '0.6rem', width: '100%', alignItems: 'stretch' }}>
                      <div style={{ flexGrow: 1, display: 'flex', background: 'white', borderRadius: '8px', border: '2px solid #cbd5e1', overflow: 'hidden' }}>
                          <div style={{ background: '#059669', width: '38px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', color: 'white', fontWeight: 'bold', fontSize: '0.75rem', paddingBottom: '0.3rem' }}>
-                             <div style={{ display: 'flex', gridTemplateColumns: 'repeat(3, 1fr)', width: '22px', height: '22px', gap: '2px', display: 'grid', justifyContent: 'center', alignContent: 'center', marginTop: '6px', marginBottom: '4px' }}>, alignContent: 'center', marginTop: '6px', marginBottom: '4px' }}>
+                             <div style={{ display: 'flex', gridTemplateColumns: 'repeat(3, 1fr)', width: '22px', height: '22px', gap: '2px', display: 'grid', justifyContent: 'center', alignContent: 'center', marginTop: '6px', marginBottom: '4px' }}>
                                {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((_, i) => <div key={i} style={{ width: '6px', height: '6px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>{i !== 4 && <Zap size={6} fill="#fde047" color="#fde047" strokeWidth={1} />}</div>)}
                             </div>
                             FIN
