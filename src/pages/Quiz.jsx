d913326afdfe9de22dfc06070e4d21b4ec0c1d59
miinin <miika.miinin@edu.lappeenranta.fi>
2026-04-25 22:50:47 +0300

    divfix

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1419,6 +1419,7 @@ export default function Quiz() {
                           })}
                         </div>
                       </div>
+                    </div>
                     ))}
                   </div>
                 </div>
